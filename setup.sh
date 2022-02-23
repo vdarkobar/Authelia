a858198b9827a04c6ab670dcea19641b31ecde3d
Darko Vukovic <vdarkobar@gmail.com>
2022-02-23 10:24:47 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,6 +2,7 @@
 clear
 echo -ne "${RED}Enter Time Zone: "; read TZONE; \
 echo -ne "${RED}Enter Authelia Port Number (9091): "; read PORTN; \
+echo -ne "${RED}Enter Redis Port Number (6379): "; read REDPORT; \
 echo -ne "${RED}Enter Domain name: "; read DNAME; \
 echo -ne "${RED}Enter Authelia Subomain: "; read SUBDNAME; \
 echo -ne "${RED}Enter Authentification Notifier Email: "; read ANEMAIL; \
@@ -12,6 +13,7 @@ echo -ne "${RED}Enter Authelia Administrator Email: "; read USEREMAIL1; \
 echo -ne "${RED}Enter Authelia Administrator Password: "; read AAUPASS; \
 SALT=$(openssl rand -base64 32) \
 HASHED_PASSWORD=$(echo -n $AAUPASS| argon2 $SALT -k 1024 -t 1 -p 8 -e -id); sed -i "s|CHANGE_HASHED_PASSWORD1|${HASHED_PASSWORD}|" users_database.yml && \
+REDIS_PASSWORD=$(tr -dc A-Za-z0-9 </dev/urandom | head -c 32); sed -i "s|CHANGE_REDIS_PASSWORD|${REDIS_PASSWORD}|" .env && \
 echo | tr -dc A-Za-z0-9 </dev/urandom | head -c 63 > secrets/authelia_jwt_secret && \
 echo | tr -dc A-Za-z0-9 </dev/urandom | head -c 63 > secrets/authelia_session_secret && \
 echo | tr -dc A-Za-z0-9 </dev/urandom | head -c 32 > secrets/authelia_storage_encryption_key && \
@@ -19,6 +21,7 @@ echo | tr -dc A-Za-z0-9 </dev/urandom | head -c 32 > secrets/authelia_storage_my
 echo | tr -dc A-Za-z0-9 </dev/urandom | head -c 32 > secrets/mysql_root_password && \
 sed -i "s|01|${TZONE}|" .env && \
 sed -i "s|02|${PORTN}|" .env && \
+sed -i "s|03|${REDPORT}|" .env && \
 sed -i "s|CHANGE-DOMAINNAME|${DNAME}|" configuration.yml && \
 sed -i "s|CHANGE-SUBDOMAIN|${SUBDNAME}|" configuration.yml && \
 sed -i "s|CHANGE-EMAIL|${ANEMAIL}|" configuration.yml && \
